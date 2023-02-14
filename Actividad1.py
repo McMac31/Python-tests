18b3867538f50cec95aa17328e4d9e2abd7df8a3
McMac31 <125380860+McMac31@users.noreply.github.com>
2023-02-14 16:19:11 +0100

    Actividades 1

---
+++ Actividad1.py
@@ -0,0 +1,22 @@
+# Salario= int(input("¿Cuanto cobras?"))
+# print(f"Cobrarias {Salario*10} si fueras experto en python 🤣"
+
+# Valor1 = int(input("Ingrese el primer valor a sumar. "))
+# Valor2= int(input("Ingrese el segundo valor a sumar. "))
+# print(f"El resultado es {Valor1+Valor2}")
+
+# personas= int(input("¿Cuantas personas son? "))
+# preciototal= int(input("¿Cual es el precio total? "))
+# print(f"Cada uno debe pagar{preciototal/personas} ")
+
+# simbolo= input("Teclea k si quieres pasar kilos a libras/ teclea l si quieres pasar de libras a kilos ")
+
+# if simbolo == "k":
+#     k= float(input("ingresa los kilogramos"))
+#     print(f"Su resultado en libras es {k*2.2046}")
+
+# elif simbolo == "l":
+#     l= float(input("Ingresa las libras"))
+#     print(f"Su resultado en kilogramos es{l/2.2046}")
+# else:
+#     print("Ingrese una letra valida")
