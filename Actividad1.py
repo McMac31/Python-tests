e047ae6472a472079763bbff8f18e0b35d279b9e
McMac31 <125380860+McMac31@users.noreply.github.com>
2023-02-14 16:34:38 +0100

    Update Actividad1.py

--- Actividad1.py
+++ Actividad1.py
@@ -1,22 +1,22 @@
-# Salario= int(input("¿Cuanto cobras?"))
-# print(f"Cobrarias {Salario*10} si fueras experto en python 🤣")
+ Salario= int(input("¿Cuanto cobras?"))
+print(f"Cobrarias {Salario*10} si fueras experto en python 🤣")
 
-# Valor1 = int(input("Ingrese el primer valor a sumar. "))
-# Valor2= int(input("Ingrese el segundo valor a sumar. "))
-# print(f"El resultado es {Valor1+Valor2}")
+ Valor1 = int(input("Ingrese el primer valor a sumar. "))
+ Valor2= int(input("Ingrese el segundo valor a sumar. "))
+ print(f"El resultado es {Valor1+Valor2}")
 
-# personas= int(input("¿Cuantas personas son? "))
-# preciototal= int(input("¿Cual es el precio total? "))
-# print(f"Cada uno debe pagar{preciototal/personas} ")
+ personas= int(input("¿Cuantas personas son? "))
+ preciototal= int(input("¿Cual es el precio total? "))
+ print(f"Cada uno debe pagar{preciototal/personas} ")
 
-# simbolo= input("Teclea k si quieres pasar kilos a libras/ teclea l si quieres pasar de libras a kilos ")
+ simbolo= input("Teclea k si quieres pasar kilos a libras/ teclea l si quieres pasar de libras a kilos ")
 
-# if simbolo == "k":
-#     k= float(input("ingresa los kilogramos"))
-#     print(f"Su resultado en libras es {k*2.2046}")
+ if simbolo == "k":
+     k= float(input("ingresa los kilogramos"))
+     print(f"Su resultado en libras es {k*2.2046}")
 
-# elif simbolo == "l":
-#     l= float(input("Ingresa las libras"))
-#     print(f"Su resultado en kilogramos es{l/2.2046}")
-# else:
-#     print("Ingrese una letra valida")
+ elif simbolo == "l":
+     l= float(input("Ingresa las libras"))
+     print(f"Su resultado en kilogramos es{l/2.2046}")
+ else:
+     print("Ingrese una letra valida")
